bcc693f856821e77d9d4ffe38a31b4437c29cd25
RIAstar <maxime@riastar.net>
2014-11-12 18:22:55 +0100

    ported "mako" template compilation to grunt with "nunjucks"

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,6 @@
+var nunjucks = require('nunjucks'),
+    path = require('path');
+
 module.exports = function (grunt) {
 
     /**
@@ -9,6 +12,36 @@ module.exports = function (grunt) {
      * Load NPM grunt tasks.
      */
     require('load-grunt-tasks')(grunt, {pattern: ['grunt-*']});
+
+    function nunjucksTask() {
+        var options = this.options();
+
+        this.files.forEach(function (f) {
+            var filepath = path.resolve(__dirname, f.src[0]);
+
+            if (!grunt.file.exists(filepath)) {
+                grunt.log.warn('Template`s file "' + filepath + '" not found.');
+                return false;
+            }
+
+            if (!options.data) {
+                grunt.log.warn('Template`s data is empty. Guess you forget to specify data option');
+            }
+
+            var data = (typeof options.preprocessData === 'function')
+                ? options.preprocessData.call(f, options.data || {})
+                : options.data || {};
+
+            var template = grunt.file.read(filepath);
+
+            nunjucks.configure(options.paths || '', options);
+
+            var compiledHtml = nunjucks.renderString(template, data);
+
+            grunt.file.write(f.dest, compiledHtml);
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
+    }
 
     grunt.initConfig({
         closureDepsWriter: {
@@ -30,13 +63,51 @@ module.exports = function (grunt) {
                     'src/style/app.css': 'src/style/app.less'
                 }
             }
+        },
+
+        nunjucks: {
+            options: {
+                tags: {
+                    variableStart: '${',
+                    variableEnd: '}'
+                }
+            },
+            dev: {
+                options: {
+                    data: {
+                        "versionslashed": "",
+                        "apache_base_path": "localhost/",
+                        "api_url": "//mf-chsdi3.dev.bgdi.ch",
+                        "device": "desktop",
+                        "mode": "dev"
+                    }
+                },
+                files: {
+                    'src/index.html': 'src/index.mako.html'
+                }
+            },
+            devMobile: {
+                options: {
+                    data: {
+                        "versionslashed": "",
+                        "apache_base_path": "localhost/",
+                        "api_url": "//mf-chsdi3.dev.bgdi.ch",
+                        "device": "mobile",
+                        "mode": "dev"
+                    }
+                },
+                files: {
+                    'src/mobile.html': 'src/index.mako.html'
+                }
+            }
         }
     });
 
+    grunt.registerMultiTask('nunjucks', 'Renders nunjucks template to HTML', nunjucksTask);
     grunt.registerTask(
         'dev',
         'Builds the files required for development',
-        ['closureDepsWriter:dev', 'less:dev']
+        ['closureDepsWriter:dev', 'less:dev', 'nunjucks:dev', 'nunjucks:devMobile']
     );
     grunt.registerTask('default', 'Default task: build dev environment', ['dev']);
 
