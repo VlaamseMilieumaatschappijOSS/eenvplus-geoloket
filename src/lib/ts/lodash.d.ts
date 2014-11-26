719844f5d1b6586d81c08493d6366b15c19d6580
RIAstar <maxime@riastar.net>
2014-11-26 15:15:28 +0100

    enhanced definition structure

--- src/lib/ts/lodash.d.ts
+++ src/lib/ts/lodash.d.ts
@@ -2,12 +2,28 @@ declare var _:_.Static;
 
 declare module _ {
 
+    /** Chaining */
     interface Static {
         (value:any):Chain;
+        chain(value:any):Chain;
+        toString():string;
+        value():any;
+        valueOf():any;
+    }
 
+    /** Arrays */
+    interface Static {
         first(array:any[], fn?:any, scope?:any):any;
         last(array:any[], fn?:any, scope?:any):any;
+    }
 
+    interface Chain {
+        first(fn?:any, scope?:any):Chain;
+        last(fn?:any, scope?:any):Chain;
+    }
+
+    /** Collections */
+    interface Static {
         cloneDeep(value:any, fn?:(value:any) => any, scope?:any):any;
         each(collection:any, fn:any, scope?:any):any;
         filter(collection:any, fn:any, scope?:any):any[];
@@ -15,32 +31,37 @@ declare module _ {
         invoke(collection:any, fn:Function, ...args:any[]):any;
         invoke(collection:any, methodName:string, ...args:any[]):any;
         map(collection:any, fn:any, scope?:any):any;
-        mapValues(collection:any, fn:any, scope?:any):any;
         reduce(collection:any, fn:MemoIterator, accumulator:any, scope?:any):any;
+    }
 
+    interface Chain {
+        each(fn:any, scope?:any):Chain;
+        filter(fn:any, scope?:any):Chain;
+        invoke(fn:Function, ...args:any[]):Chain;
+        invoke(methodName:string, ...args:any[]):Chain;
+        map(fn:any, scope?:any):Chain;
+    }
+
+    /** Functions */
+    interface Static {
         bind(fn:Function, scope?:any, ...args:any[]):Function;
         curry(fn:Function):Function;
         partial(fn:Function, ...args:any[]):Function;
         partialRight(fn:Function, ...args:any[]):Function;
+    }
 
+    /** Objects */
+    interface Static {
+        mapValues(object:Object, fn:any, scope?:any):any;
     }
 
     interface Chain {
-
-        first(fn?:any, scope?:any):Chain;
-        last(fn?:any, scope?:any):Chain;
-
-        each(fn:any, scope?:any):Chain;
-        filter(fn:any, scope?:any):Chain;
-        invoke(fn:Function, ...args:any[]):any;
-        invoke(methodName:string, ...args:any[]):any;
-        map(fn:any, scope?:any):Chain;
         mapValues(fn:any, scope?:any):Chain;
-
     }
 
+    /** Iterators */
     interface MemoIterator {
-        (prev:any, curr:any, key?:any, collection?:any): boolean;
+        (prev:any, curr:any, key?:any, collection?:any):boolean;
     }
 
 }
