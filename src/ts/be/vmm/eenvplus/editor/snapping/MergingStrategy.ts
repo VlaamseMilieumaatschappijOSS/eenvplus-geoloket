34e12d03bff484274985f1d96b87c2831806664d
RIAstar <maxime@riastar.net>
2015-01-23 00:15:48 +0100

    [refactor] MergingStrategy to use SnappingMonitor

--- src/ts/be/vmm/eenvplus/editor/snapping/MergingStrategy.ts
+++ src/ts/be/vmm/eenvplus/editor/snapping/MergingStrategy.ts
@@ -4,23 +4,17 @@
 module be.vmm.eenvplus.editor.snapping {
     'use strict';
 
-    MergingStrategy.$inject = ['epMap', 'epSnappingState', 'epSnappingStore'];
+    MergingStrategy.$inject = ['epMap', 'epSnappingState', 'epSnappingMonitor'];
 
-    function MergingStrategy(map:ol.Map, state:StateController<SnappingType>, store:SnappingStore):void {
+    function MergingStrategy(map:ol.Map, state:StateController<SnappingType>, monitor:SnappingMonitor):void {
 
         /* ------------------ */
         /* --- properties --- */
         /* ------------------ */
 
         var painter:DrawPrivate,
-            nodes:ol.source.Vector,
-            snapping:boolean,
-            protoPainter:DrawPrivate = <DrawPrivate> ol.interaction.Draw.prototype,
-            toPixel = map.getPixelFromCoordinate.bind(map);
-
-        function setResolution(resolution:number):void {
-            if (painter) painter.snapTolerance_ = resolution;
-        }
+            superPointerMove,
+            superAddToDrawing;
 
 
         /* -------------------- */
@@ -28,35 +22,6 @@ module be.vmm.eenvplus.editor.snapping {
         /* -------------------- */
 
         state(SnappingType.MERGE, activate, deactivate);
-        store.resolutionChanged.add(setResolution);
-
-
-        /* ---------------------- */
-        /* --- event handlers --- */
-        /* ---------------------- */
-
-        function handleMouseMove(event:ol.MapBrowserPointerEvent):void {
-            var snappedCoordinate = calculateCoordinate(event.coordinate);
-            invalidateGeometrySnapping(event.coordinate, snappedCoordinate);
-
-            event.coordinate = snappedCoordinate;
-            protoPainter.handlePointerMove_.call(painter, event);
-        }
-
-        function addToDrawing(event:ol.MapBrowserPointerEvent):void {
-            protoPainter.addToDrawing_.call(painter, event);
-            if (snapping) painter.finishDrawing_();
-        }
-
-        function abortDrawing():ol.Feature {
-            snapping = false;
-            return protoPainter.abortDrawing_.call(painter);
-        }
-
-
-        /* ----------------- */
-        /* --- behaviour --- */
-        /* ----------------- */
 
         function activate() {
             painter = _.find(map.getInteractions().getArray(), isActivePainter);
@@ -65,44 +30,80 @@ module be.vmm.eenvplus.editor.snapping {
                 return;
             }
 
-            painter.snapTolerance_ = store.resolution;
-            painter.handlePointerMove_ = handleMouseMove;
+            painter.snapTolerance_ = 1;
+            painter.handlePointerMove_ = handlePointerMove;
             painter.addToDrawing_ = addToDrawing;
-            painter.abortDrawing_ = abortDrawing;
-            nodes = feature.getLayer(map, feature.FeatureType.NODE).getSource();
+
+            var proto = <DrawPrivate> ol.interaction.Draw.prototype;
+            superPointerMove = proto.handlePointerMove_.bind(painter);
+            superAddToDrawing = proto.addToDrawing_.bind(painter);
+
+            monitor.moveAtEnd.add(moveAtPoint);
+            monitor.moveAtStart.add(moveAtPoint);
+            monitor.moveOutside.add(superPointerMove);
         }
+
+
+        /* ----------------- */
+        /* --- overrides --- */
+        /* ----------------- */
+
+        /**
+         * Pass all mouse move events down to the SnappingMonitor for analysis of current snapping possibilities.
+         *
+         * @override
+         * @see ol.interaction.Draw#handlePointerMove_
+         * @see SnappingMonitor#update
+         */
+        function handlePointerMove(event:ol.MapBrowserPointerEvent):void {
+            monitor.update(event, painter.sketchFeature_);
+        }
+
+        /**
+         * Finish drawing when the user single-snap-clicks to an end point.
+         *
+         * @override
+         * @see ol.interaction.Draw#addToDrawing_
+         * @see ol.interaction.Draw#finishDrawing_
+         */
+        function addToDrawing(event:SnappingPointerEvent):void {
+            superAddToDrawing(event);
+            if (event.end) painter.finishDrawing_();
+        }
+
+
+        /* ---------------------- */
+        /* --- event handlers --- */
+        /* ---------------------- */
+
+        function moveAtPoint(event:SnappingPointerEvent):void {
+            event.coordinate = event.snappedCoordinate;
+            superPointerMove(event);
+        }
+
+
+        /* ----------------- */
+        /* --- behaviour --- */
+        /* ----------------- */
 
         function isActivePainter(interaction:ol.interaction.Interaction):boolean {
             return interaction instanceof ol.interaction.Draw && interaction.getActive();
         }
 
-        function calculateCoordinate(coordinate:ol.Coordinate):ol.Coordinate {
-            var closestNode = nodes.getClosestFeatureToCoordinate(coordinate),
-                closestNodeCoordinate = (<ol.geometry.SimpleGeometry> closestNode.getGeometry()).getFirstCoordinate(),
-                pixels = [coordinate, closestNodeCoordinate].map(toPixel),
-                distance = Math.sqrt(apply(ol.coordinate.squaredDistance)(pixels));
 
-            return distance > painter.snapTolerance_ ? coordinate : closestNodeCoordinate;
-        }
-
-        function invalidateGeometrySnapping(mouseCoordinate:ol.Coordinate, snappedCoordinate:ol.Coordinate):void {
-            if (!painter.sketchFeature_) return;
-
-            if (ol.coordinate.equals(mouseCoordinate, snappedCoordinate)) {
-                if (snapping) snapping = false;
-            }
-            else snapping = true;
-        }
+        /* ------------------- */
+        /* --- destruction --- */
+        /* ------------------- */
 
         function deactivate() {
+            monitor.moveAtEnd.remove(moveAtPoint);
+            monitor.moveAtStart.remove(moveAtPoint);
+            monitor.moveOutside.remove(superPointerMove);
+
             if (!painter) return;
 
-            if (painter.handlePointerMove_ === handleMouseMove)
-                painter.handlePointerMove_ = protoPainter.handlePointerMove_.bind(painter);
-            if (painter.addToDrawing_ === addToDrawing)
-                painter.addToDrawing_ = protoPainter.addToDrawing_.bind(painter);
-            if (painter.abortDrawing_ === abortDrawing)
-                painter.abortDrawing_ = protoPainter.abortDrawing_.bind(painter);
+            if (painter.handlePointerMove_ === handlePointerMove) painter.handlePointerMove_ = superPointerMove;
+            if (painter.addToDrawing_ === addToDrawing) painter.addToDrawing_ = superAddToDrawing;
             painter = null;
         }
 
